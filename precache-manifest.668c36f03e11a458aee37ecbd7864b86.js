45292a03e295fd18f84a0cd48146efe00a158729
Timtho <maciej.karasinski.95@gmail.com>
2020-02-22 17:36:52 +0100

    Updates

---
+++ precache-manifest.668c36f03e11a458aee37ecbd7864b86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b590c13eea4e84ad797b222f109d614",
+    "url": "/Videos/index.html"
+  },
+  {
+    "revision": "531c51f91a7cb3ffa01b",
+    "url": "/Videos/static/css/main.8ad48a67.chunk.css"
+  },
+  {
+    "revision": "e17bf3573f86edd36580",
+    "url": "/Videos/static/js/2.c2ec2402.chunk.js"
+  },
+  {
+    "revision": "6b1c9aa1f24077e4bfd4b51db2988c14",
+    "url": "/Videos/static/js/2.c2ec2402.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "531c51f91a7cb3ffa01b",
+    "url": "/Videos/static/js/main.e59dc8cf.chunk.js"
+  },
+  {
+    "revision": "b2bf0ce44d6ffb9a495a",
+    "url": "/Videos/static/js/runtime-main.e608b381.js"
+  }
+]);
